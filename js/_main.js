b1cd455a18097f72ce8fef4729804305d5a7f7e6
Han Heul Jo <hjo8@illinois.edu>
2014-11-02 17:29:49 -0600

    attempted git integration with framework

--- js/_main.js
+++ js/_main.js
@@ -12,3 +12,32 @@ function showValue(newValue)
 {
 	document.getElementById("range").innerHTML=newValue;
 }
+
+var userInput = function() {
+
+}
+
+function getTags() {
+    document.getElementById("tags").submit();
+}
+
+/**
+var loudness = null; 
+var inputElements = document.getElementsByClassName('loudness');
+for(var i=0; inputElements[i]; ++i){
+      if(inputElements[i].checked){
+           checkedValue = inputElements[i].value;
+           break;
+      }
+}
+*/
+
+function showTags() {
+    var x = document.getElementById("tags");
+    var text = "";
+    var i;
+    for (i = 0; i < x.length ;i++) {
+        text += x.elements[i].value + "<br>";
+    }
+    document.getElementById("demo").innerHTML = text;
+}
