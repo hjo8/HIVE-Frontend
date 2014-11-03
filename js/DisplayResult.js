beb4e37198fe9a51374dfddec0d5a29fcff31d92
Han Heul Jo <hjo8@illinois.edu>
2014-11-03 13:46:13 -0600

    Fixed Album Artwork Change Issue

--- js/DisplayResult.js
+++
@@ -1,5 +0,0 @@
-$(function(){
-   $('#song2').on('focus mouseenter', function(){
-    $(this).parent().css("background", "url(http://www.isgoodstuff.com/wp-content/uploads/2008/10/genre-hiphop.jpg)");
-   });
-});
