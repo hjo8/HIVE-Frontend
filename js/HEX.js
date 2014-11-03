7b78f638b6ced43e590dd1fd21c7855cc8e2856c
Han Heul Jo <hjo8@illinois.edu>
2014-11-03 14:36:13 -0600

    Finalize Results Page
    Still need to connect to JSON files

--- js/HEX.js
+++ js/HEX.js
@@ -1,5 +1,31 @@
+var song0Album = "url(http://www.hwdyk.com/q/quizimage/albumcovertrivia.jpg)";
+var song1Album = "url(http://www.amiright.com/album-cover-themes/images/album-ACDC-BLACK-ICE.jpg)";
+var song2Album = "url(http://c0903002.cdn.cloudfiles.rackspacecloud.com/9417-coldplay_ghost_stories_album_artwork.jpg)";
+var song3Album = "url(http://archive.wired.com/images/article/full/2009/02/abbey_road.jpg)";
+var song4Album = "url(http://www.tomlohrmannmusic.com/blog/wp-content/uploads/2012/09/muse-the-resistance-album-artwork.jpeg)";
+var song5Album = "url(http://www.smashingmagazine.com/images/music-cd-covers/30.jpg)";
+var song6Album = "url(http://www.tomlohrmannmusic.com/blog/wp-content/uploads/2012/09/nirvana_nevermind_album_cover.jpeg)";
+
+/** to change on hover, also relocate id to div with text
 $(function(){
-   $('#song2').on('focus mouseenter', function(){
-    $(this).parent().css("background", "url(http://www.isgoodstuff.com/wp-content/uploads/2008/10/genre-hiphop.jpg) center center");
+   $('#song0').on('focus mouseenter', function(){
+    $(this).parent().css("background", song0Album);
    });
 });
+*/
+
+document.getElementById("song0").style.background = song0Album + " center center";
+document.getElementById("song1").style.background = song1Album + " center center";
+document.getElementById("song2").style.background = song2Album + " center center";
+document.getElementById("song3").style.background = song3Album + " center center";
+document.getElementById("song4").style.background = song4Album + " center center";
+document.getElementById("song5").style.background = song5Album + " center center";
+document.getElementById("song6").style.background = song6Album + " center center";
+
+document.getElementById("song0").style.backgroundSize="230px 230px";
+document.getElementById("song1").style.backgroundSize="230px 230px";
+document.getElementById("song2").style.backgroundSize="230px 230px";
+document.getElementById("song3").style.backgroundSize="230px 230px";
+document.getElementById("song4").style.backgroundSize="230px 230px";
+document.getElementById("song5").style.backgroundSize="230px 230px";
+document.getElementById("song6").style.backgroundSize="230px 230px";
