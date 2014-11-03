9d7193e1b4407b7da9af2ab991b7e03d81a194c9
Han Heul Jo <hjo8@illinois.edu>
2014-11-02 23:17:01 -0600

    Album Artwork Change with JS
    Somewhat works, but needs more work with formatting the display

---
+++ js/DisplayResult.js
@@ -0,0 +1,5 @@
+$(function(){
+   $('#song2').on('focus mouseenter', function(){
+    $(this).parent().css("background", "url(http://www.isgoodstuff.com/wp-content/uploads/2008/10/genre-hiphop.jpg)");
+   });
+});
