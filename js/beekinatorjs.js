b847faff86d3909ae8d80804600e8586e2a81596
czhng100 <czhng100@illinois.edu>
2014-12-04 22:37:11 -0600

    http request attempt

--- js/beekinatorjs.js
+++ js/beekinatorjs.js
@@ -1,43 +1,47 @@
 var myArray = [
 {
-"num": "Q1",
-"context": "Is this song sung by a female artist?"
+	"num": "Q1",
+	"context": "Is this song sung by a female artist?"
 },
 {
-"num": "Q2",
-"context": "Is this song fast?"
+	"num": "Q2",
+	"context": "Is this song fast?"
 },
 {
-"num": "Q3",
-"context": "Is the genre country?"
+	"num": "Q3",
+	"context": "Is the genre country?"
 },
 {
-"num": "Q4",
-"context": "Is the genre jazz?"
+	"num": "Q4",
+	"context": "Is the genre jazz?"
 },
 {
-"num": "Q5",
-"context": "Is the genre pop?"
+	"num": "Q5",
+	"context": "Is the genre pop?"
 },
 {
-"num": "Q6",
-"context": "Is the genre heavy metal?"
+	"num": "Q6",
+	"context": "Is the genre heavy metal?"
 },
 {
-"num": "Q7",
-"context": "Do the instruments include a piano?"
+	"num": "Q7",
+	"context": "Do the instruments include a piano?"
 },
 {
-"num": "Q8",
-"context": "Do the instruments include a guitar?"
+	"num": "Q8",
+	"context": "Do the instruments include a guitar?"
 },
 {
-"num": "Q9",
-"context": "Do the instruments include drums?"
+	"num": "Q9",
+	"context": "Do the instruments include drums?"
 },
 {
-"num": "Q10",
-"context": "Is this song sung by a single person?"
+	"num": "Q10",
+	"context": "Is this song sung by a single person?"
+},
+{
+	"num": "I'm done asking questions!",
+	"context": "Please click 'Submit My Choices'"
 }
 ]
 //myArray contains the questions. To be completed
@@ -59,8 +63,6 @@ myFunction(myArray);
 YesButton.addEventListener("click", clickedY);
 NoButton.addEventListener("click", clickedN);
 displayChoices.addEventListener("click", showChoices);
-next.addEventListener("click", goToNext);
-previous.addEventListener("click", goToPrevious);
 
 function clickedY(eve){
 	if ( i == 0 )
@@ -77,7 +79,7 @@ function clickedY(eve){
 		tagList.push("heavy metal");
 	if ( i == 6 )
 		tagList.push("piano");
-	if ( i == 7 )
+	if ( i == 7 )			
 		tagList.push("guitar");
 	if ( i == 8 )
 		tagList.push("drums");
@@ -94,10 +96,37 @@ function clickedN(eve){
 		tagList.push("group");
 	i++;
 	myFunction(myArray);
-
 }//needs to add data process and transport functions.
 
 function showChoices(){
 	var text = tagList;
     document.getElementById("choicesList").innerHTML = text;
+}
+
+var tag_json = {
+	"type":"tags",
+	"content":tagList,
+};
+
+function loadXMLDoc()
+{
+var xmlhttp;
+if (window.XMLHttpRequest)
+  {// code for IE7+, Firefox, Chrome, Opera, Safari
+  xmlhttp=new XMLHttpRequest();
+  }
+else
+  {// code for IE6, IE5
+  xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+  }
+xmlhttp.onreadystatechange=function()
+  {
+  if (xmlhttp.readyState==4 && xmlhttp.status==200)
+    {
+    document.getElementById("myDiv").innerHTML=xmlhttp.responseText;
+    }
+  }
+xmlhttp.open("POST",response.txt,true);
+xmlhttp.setRequestHeader("Content-type", "application/json");
+xmlhttp.send(JSON.stringify(tag_json));
 }
